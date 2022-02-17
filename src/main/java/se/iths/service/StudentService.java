f964ebbc45928caf3a16a0a0501fb685a561d817
HelenaHalldinITHS <helena.halldin@iths.se>
2022-02-17 11:17:40 +0100

    Add method for creating a student in StudentService

--- src/main/java/se/iths/service/StudentService.java
+++ src/main/java/se/iths/service/StudentService.java
@@ -1,5 +1,7 @@
 package se.iths.service;
 
+import se.iths.entity.Student;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
@@ -9,4 +11,9 @@ public class StudentService {
 
     @PersistenceContext
     EntityManager entityManager;
+
+    public Student createStudent(Student student){
+        entityManager.persist(student);
+        return student;
+    }
 }
