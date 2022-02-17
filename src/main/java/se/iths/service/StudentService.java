df29f57d6c031cc830d57fe57ed7a2b2f5162d88
HelenaHalldinITHS <helena.halldin@iths.se>
2022-02-17 11:12:38 +0100

    Make StudentService Transactional + add EntityManager

--- src/main/java/se/iths/service/StudentService.java
+++ src/main/java/se/iths/service/StudentService.java
@@ -1,7 +1,12 @@
 package se.iths.service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
+@Transactional
 public class StudentService {
 
-
+    @PersistenceContext
+    EntityManager entityManager;
 }
