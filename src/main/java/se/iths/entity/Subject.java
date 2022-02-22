02c6fb98baf4475a2375da42c555754c08a5b75e
HelenaHalldinITHS <helena.halldin@iths.se>
2022-02-22 13:10:26 +0100

    Add entity Subject

---
+++ src/main/java/se/iths/entity/Subject.java
@@ -0,0 +1,30 @@
+package se.iths.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Subject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+    String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
